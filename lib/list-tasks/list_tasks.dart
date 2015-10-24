9b6c60c84b602f16677308e1b16cc6fa6d46e7da
mswift42 <martin.haesler@gmail.com>
2015-10-24 09:17:35 +0100

    register polymer-element list-tasks.

--- lib/list-tasks/list_tasks.dart
+++ lib/list-tasks/list_tasks.dart
@@ -4,3 +4,12 @@ library polytasksdart.lib.list_tasks.list_tasks;
 
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart';
+import 'package:polytasksdart/list-task/list_task.dart';
+
+@PolymerRegister('list-tasks')
+class ListTasks extends PolymerElement {
+  ListTasks.created() : super.created();
+
+  @property
+  List<ListTask> tasklist;
+}
