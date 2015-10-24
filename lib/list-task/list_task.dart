89747ba1cb21c026a666bf380ffc78653a483d1d
mswift42 <martin.haesler@gmail.com>
2015-10-24 09:08:20 +0100

    Import paper-elements.

---
+++ lib/list-task/list_task.dart
@@ -0,0 +1,11 @@
+@HtmlImport('list_taksk.html')
+library polytasksdart.lib.list_task.list_task;
+
+import 'package:polymer_elements/paper_dialog.dart';
+import 'package:polymer_elements/paper_button.dart';
+import 'package:polymer_elements/paper_material.dart';
+import 'package:polymer_elements/paper_input.dart';
+import 'package:polymer_elements/paper_checkbox.dart';
+import 'package:polymer_elements/paper_icon_button.dart';
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart';
